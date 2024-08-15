12faf6e648d5524e32e9669e12cd01de75a2acc5
Jeanne d'Arc NYIRANWIZA <njoanc@gmail.com>
2024-08-15 19:47:31 +0200

    add():enable webcamera

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
-/**
- * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
+const video = document.getElementById("webcam");
+const liveView = document.getElementById("liveView");
+const demoSection = document.getElementById("demos");
+const enableWebcamButton = document.getElementById("webcamButton");
 
-const status = document.getElementById("status");
-if (status) {
-  status.innerText = "Loaded TensorFlow.js - version: " + tf.version.tfjs;
+//check if webcam access is supported
+function getUserMediaSupported() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 }
+
+//If webcam supported, add event listener to button for when user
+//wants to activate it to call enableCam function which we will define in the next step
+
+if (getUserMediaSupported()) {
+  enableWebcamButton.addEventListener("click", enableCam);
+} else {
+  console.warn("getUserMedia() is not supported by your browser");
+}
+
+//Ebale the live webcam view and start classification
+function enableCam(event) {
+  //Only continue if the COCO-SSD has finished loading
+  if (!model) {
+    return;
+  }
+
+  //hide the button once clicked
+  event.target.classList.add("removed");
+
+  //getUserMedia parameters to force video but not audio
+
+  const constraints = {
+    video: true,
+  };
+
+  //Activate the webcam stream
+
+  navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    video.srcObject = stream;
+    video.addEventListener("loadeddata", predictWebcam);
+  });
+}
+
+//Placeholder model function for next step
+
+function predictWebcam() {}
+
+//Pretend model has loaded so we can try out the webcam code
+
+var model = true;
+
+demoSection.classList.remove("invisible");
